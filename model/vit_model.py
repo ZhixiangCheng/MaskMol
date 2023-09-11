73aee9cd7825863fa2c8f000d986490f4b850ef7
ZhixiangCheng <63796912+ZhixiangCheng@users.noreply.github.com>
2023-09-11 13:33:48 +0800

    Update vit_model.py

--- model/vit_model.py
+++ model/vit_model.py
@@ -13,19 +13,6 @@ class MaskMol(nn.Module):
         self.motif_classes = motif_classes
 
         self.vit = timm.create_model("vit_base_patch16_224", pretrained=pretrained, num_classes=0)
-        # self.vit = timm.create_model("vit_base_patch16_224", num_classes=0)
-
-        # # Change the number of attention heads in each layer to 6
-        # num_heads = 6
-        # for layer in self.vit .blocks:
-        #     layer.attn = timm.models.vision_transformer.Attention(
-        #         dim=layer.attn.dim,
-        #         num_heads=num_heads,
-        #         qkv_bias=layer.attn.qkv_bias,
-        #         qk_scale=layer.attn.qk_scale,
-        #         attn_drop=layer.attn.attn_drop,
-        #         proj_drop=layer.attn.proj_drop,
-        #     )
 
         # Remove the classification head to get features only
         self.vit.head = nn.Identity()
@@ -44,15 +31,12 @@ class MaskMol(nn.Module):
         y = y[:, 1:, :]  # (batch_size, 196, 768)
 
         atom_patch_outputs = self.atom_patch_classifier(y)  # Shape: (batch_size, num_patches, num_classes)
-        # atom_patch_outputs = patch_outputs.permute(0, 2, 1)    # Shape: (batch_size, num_classes, num_patches)
 
         # Mask Bond task
         bond_patch_outputs = self.bond_patch_classifier(y)
-        # bond_patch_outputs = patch_outputs.permute(0, 2, 1)
 
         # Mask Motif task
         motif_ouput = self.vit.forward_head(y)
         motif_outputs = self.motif_classifier(motif_ouput)
 
         return atom_patch_outputs, bond_patch_outputs, motif_outputs
-#         return bond_patch_outputs, motif_outputs
